9337a492d30e72e751fc4b61f1c26466cfa45f9e
Florian Truchot <florian@truchot.co>
2023-10-06 18:06:44 +0200

    feat: add Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+    arrowParens: "always",
+    bracketSameLine: false,
+    bracketSpacing: true,
+    semi: true,
+    singleAttributePerLine: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: false
+}
+
+module.exports = config;
