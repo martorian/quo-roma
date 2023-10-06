6e613e56430d5509e1fe59b1ad022bc6aaa2fddd
Florian Truchot <florian@truchot.co>
2023-10-06 18:18:29 +0200

    feat: add UI component

---
+++ src/components/ui/index.ts
@@ -0,0 +1,147 @@
+import { Accordion } from '@/components/ui/accordion';
+import { Alert } from '@/components/ui/alert';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { Avatar } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Card } from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Collapsible } from '@/components/ui/collapsible';
+import { Command } from '@/components/ui/command';
+import { ContextMenu } from '@/components/ui/context-menu';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { HoverCard } from '@/components/ui/hover-card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Menubar } from '@/components/ui/menubar';
+import { NavigationMenu } from '@/components/ui/navigation-menu';
+import { Popover } from '@/components/ui/popover';
+import { Progress } from '@/components/ui/progress';
+import { RadioGroup } from '@/components/ui/radio-group';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Select } from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
+import { Sheet } from '@/components/ui/sheet';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Slider } from '@/components/ui/slider';
+import { Switch } from '@/components/ui/switch';
+import { Table } from '@/components/ui/table';
+import { Tabs } from '@/components/ui/tabs';
+import { Textarea } from '@/components/ui/textarea';
+import { Toast } from '@/components/ui/toast';
+import { Toaster } from '@/components/ui/toaster';
+import { Toggle } from '@/components/ui/toggle';
+import { Tooltip } from '@/components/ui/tooltip';
+
+export const UI = {
+    Accordion: Accordion,
+    Alert: Alert,
+    AlertDialog: AlertDialog,
+    AlertDialogAction: AlertDialogAction,
+    AlertDialogCancel: AlertDialogCancel,
+    AlertDialogContent: AlertDialogContent,
+    AlertDialogDescription: AlertDialogDescription,
+    AlertDialogFooter: AlertDialogFooter,
+    AlertDialogHeader: AlertDialogHeader,
+    AlertDialogTitle: AlertDialogTitle,
+    AlertDialogTrigger: AlertDialogTrigger,
+    AspectRatio: AspectRatio,
+    Avatar: Avatar,
+    Badge: Badge,
+    Button: Button,
+    Calendar: Calendar,
+    Card: Card,
+    Checkbox: Checkbox,
+    Collapsible: Collapsible,
+    Command: Command,
+    ContextMenu: ContextMenu,
+    Dialog: Dialog,
+    DialogContent: DialogContent,
+    DialogHeader: DialogHeader,
+    DialogTitle: DialogTitle,
+    DialogDescription: DialogDescription,
+    DialogFooter: DialogFooter,
+    DropdownMenu: DropdownMenu,
+    DropdownMenuContent: DropdownMenuContent,
+    DropdownMenuGroup: DropdownMenuGroup,
+    DropdownMenuItem: DropdownMenuItem,
+    DropdownMenuLabel: DropdownMenuLabel,
+    DropdownMenuPortal: DropdownMenuPortal,
+    DropdownMenuSeparator: DropdownMenuSeparator,
+    DropdownMenuShortcut: DropdownMenuShortcut,
+    DropdownMenuSub: DropdownMenuSub,
+    DropdownMenuSubContent: DropdownMenuSubContent,
+    DropdownMenuSubTrigger: DropdownMenuSubTrigger,
+    DropdownMenuTrigger: DropdownMenuTrigger,
+    Form: Form,
+    FormField: FormField,
+    FormItem: FormItem,
+    FormLabel: FormLabel,
+    FormControl: FormControl,
+    FormDescription: FormDescription,
+    FormMessage: FormMessage,
+    HoverCard: HoverCard,
+    Input: Input,
+    Label: Label,
+    Menubar: Menubar,
+    NavigationMenu: NavigationMenu,
+    Popover: Popover,
+    Progress: Progress,
+    RadioGroup: RadioGroup,
+    ScrollArea: ScrollArea,
+    Select: Select,
+    Separator: Separator,
+    Sheet: Sheet,
+    Skeleton: Skeleton,
+    Slider: Slider,
+    Switch: Switch,
+    Table: Table,
+    Tabs: Tabs,
+    Textarea: Textarea,
+    Toast: Toast,
+    Toaster: Toaster,
+    Toggle: Toggle,
+    Tooltip: Tooltip,
+};
