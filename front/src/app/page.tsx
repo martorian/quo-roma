23a3b8195697fbda6f2b8848fbd822e85e0c8cee
akugone <dev@akumaw.com>
2023-10-07 11:14:54 +0200

    Add peanuts generate link

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { Peanuts } from '@/components/peanuts/peanuts';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Homepage
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Homepage
+            <Peanuts />
+        </main>
+    );
 }
