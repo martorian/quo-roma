2d70e1ba625281626453af64beafa69a673e7e26
rere <regis-graptin@sfr.fr>
2023-10-07 10:13:27 +0200

    feat: add talent layer community sismo

--- front/src/config/sismo.ts
+++ front/src/config/sismo.ts
@@ -4,8 +4,10 @@ export const sismoConfig = {
 
 export enum Channels {
     COOKING = "cooking",
+    TALENT_LAYER_COMMUNITY = "Talent Layer Community",
 }
 
 export const groupIds = {
     [Channels.COOKING]: "0xb41869ce2f5e5266808ffae60f27992e",
+    [Channels.TALENT_LAYER_COMMUNITY]: "0x22566e2202190d6cca8966782c043428",
 }
