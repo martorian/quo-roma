3bde7020ee16e9108765be6c40e9e5b08c54100d
akugone <dev@akumaw.com>
2023-10-09 15:50:50 +0200

    generate randomhandle

--- front/src/components/community/join-community-button.tsx
+++ front/src/components/community/join-community-button.tsx
@@ -1,34 +1,44 @@
 'use client';
 
-import {UI} from '@/components/ui';
-import {PropsWithChildren, useEffect, useState} from 'react';
+import { UI } from '@/components/ui';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import {
     useAccount,
     useChainId,
     usePublicClient,
-    useWalletClient
-} from "wagmi";
-import configContracts from "@/config/mumbai.json";
-import quoromaABI from "@/abis/QuoromaID.json";
-import {useParams} from "next/navigation";
-import Link from "next/link";
-import {Hash} from "lucide-react";
+    useWalletClient,
+} from 'wagmi';
+import configContracts from '@/config/mumbai.json';
+import quoromaABI from '@/abis/QuoromaID.json';
+import { useParams } from 'next/navigation';
+import Link from 'next/link';
+import { Hash } from 'lucide-react';
 
 interface Props extends PropsWithChildren {
     className?: string;
 }
 
-export const JoinCommunityButton = ({className, children}: Props) => {
-    const chainId = useChainId()
-    const publicClient = usePublicClient({chainId});
-    const {data: walletClient} = useWalletClient({chainId});
-    const {address} = useAccount({chainId});
+export const JoinCommunityButton = ({ className, children }: Props) => {
+    const chainId = useChainId();
+    const publicClient = usePublicClient({ chainId });
+    const { data: walletClient } = useWalletClient({ chainId });
+    const { address } = useAccount({ chainId });
     const [loading, setLoading] = useState<boolean>(false);
     const [hasMinted, setHasMinted] = useState<boolean>(false);
-    const {communityName} = useParams();
+    const { communityName } = useParams();
+
+    function generateRandomHandle() {
+        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        let handle = '';
+        for (let i = 0; i < 6; i++) {
+            handle += characters.charAt(
+                Math.floor(Math.random() * characters.length)
+            );
+        }
+        return handle;
+    }
 
     useEffect(() => {
-
         if (!publicClient) {
             console.log('No public client');
         }
@@ -52,16 +62,18 @@ export const JoinCommunityButton = ({className, children}: Props) => {
                 console.log('Error', e);
             }
         })();
-
-    }, [publicClient, address])
+    }, [publicClient, address]);
 
     const handleJoinCommunity = async () => {
+        const randomHandle = generateRandomHandle();
+        console.log('Random handle: ', randomHandle);
+
         console.log('Join community');
-        setLoading(true)
+        setLoading(true);
 
         if (!walletClient) {
             console.log('No wallet client');
-            setLoading(false)
+            setLoading(false);
             return;
         }
 
@@ -70,23 +82,27 @@ export const JoinCommunityButton = ({className, children}: Props) => {
                 address: configContracts.QuoromaID,
                 abi: quoromaABI,
                 functionName: 'mint',
-                args: [1, "fakehandle"],
+                args: [1, randomHandle],
                 value: BigInt(0),
             });
 
             setHasMinted(tx);
-            setLoading(false)
+            setLoading(false);
         } catch (e) {
             console.log('Error', e);
-            setLoading(false)
+            setLoading(false);
             return;
         }
-    }
+    };
 
     if (hasMinted) {
         return (
-            <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-600 flex items-center gap-2">Start to chat on
-                <UI.Button asChild className="gap-1">
+            <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-600 flex items-center gap-2">
+                Start to chat on
+                <UI.Button
+                    asChild
+                    className="gap-1"
+                >
                     <Link href={`/${communityName}`}>
                         <Hash className="w-4 h-4 inline" />
                         {communityName}
